5c5bb54b2e16d3194c7ba1e7eb229e3da06a15f7
Taha <mohamedtahasta@gmail.com>
2023-03-28 13:42:49 +0000

    nieme Commit

--- src/main/java/com/example/netflixproject1/HelloApplication.java
+++ src/main/java/com/example/netflixproject1/HelloApplication.java
@@ -20,6 +20,7 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         System.out.printf("HEllo Bitches");
+        System.out.printf("Hello fares");
         System.out.printf("Banjourrat");
         launch();
     }
