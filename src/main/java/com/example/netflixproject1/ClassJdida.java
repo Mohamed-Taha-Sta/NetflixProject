f5bac65acb32a35f0ff4fd31d4cdf37401c13d76
Taha <mohamedtahasta@gmail.com>
2023-03-28 14:07:00 +0000

    Added a class called ClassJdida and changed the formatting of the 4 SOUTS

---
+++ src/main/java/com/example/netflixproject1/ClassJdida.java
@@ -0,0 +1,40 @@
+package com.example.netflixproject1;
+
+public class ClassJdida {
+
+    private int id;
+    private String name;
+
+    public ClassJdida(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassJdida{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+
+
+}
