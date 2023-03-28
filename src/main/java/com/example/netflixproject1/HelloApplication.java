2c69c8d09b44f0a81a54da926d5dd4a37148fd12
LICHG4E <jasserhamdi17@gmail.com>
2023-03-28 13:38:06 +0100

    ADDED MSGss

--- src/main/java/com/example/netflixproject1/HelloApplication.java
+++ src/main/java/com/example/netflixproject1/HelloApplication.java
@@ -19,7 +19,7 @@ public class HelloApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.printf("HEllo MFSS");
+        System.out.printf("HEllo Bitches");
         System.out.printf("Banjourrat");
         launch();
     }
