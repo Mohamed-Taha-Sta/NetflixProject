24570e6739efa49dad169fc6f90796e1ba19d83d
Taha <mohamedtahasta@gmail.com>
2023-03-28 12:55:40 +0000

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.netflixproject1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.netflixproject1 to javafx.fxml;
+    exports com.example.netflixproject1;
+}
