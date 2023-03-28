583ba8e3f3a2fc628eb0a38b3fdae74778af0a99
LICHG4E <jasserhamdi17@gmail.com>
2023-03-28 13:00:58 +0100

    ADDED MSG

--- src/main/java/com/example/netflixproject1/HelloApplication.java
+++ src/main/java/com/example/netflixproject1/HelloApplication.java
@@ -17,7 +17,9 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+
     public static void main(String[] args) {
+        System.out.printf("HEllo MFS");
         launch();
     }
 }
