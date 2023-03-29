a60f0629f986034f2698278a82c21a22812d6e77
LICHG4E <jasserhamdi17@gmail.com>
2023-03-29 04:13:04 +0100

    now when u press sign in it gets u to user choices (actors and genre)

--- src/main/java/Controllers/RegisterPage.java
+++ src/main/java/Controllers/RegisterPage.java
@@ -2,18 +2,43 @@ package Controllers;
 
 import com.example.netflixproject.HelloApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-public class RegisterPage {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterPage implements Initializable {
+    public DatePicker UserBirthday;
+    public TextField UserPassword;
+    public TextField UserEmail;
+    public TextField UserPrename;
+    public TextField UserName;
+    public ListView<String> ActorsList=new ListView<>();
     @FXML
     private Button SignUp;
 
     @FXML
     private Button login;
-
+    ResourceBundle BN;
+    URL ul;
     @FXML
     protected void OnSignIn() throws Exception{
         HelloApplication.SetRoot("LoginPage");
     }
+    @FXML
+    protected void OnSignUp() throws Exception{
+        HelloApplication.SetRoot("ChoicesMenu");
+        initialize(ul,BN);
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String[] actors = {"Tom Hanks", "Meryl Streep", "Leonardo DiCaprio", "Emma Stone", "Robert Downey Jr.","Robert Downey Jr.","Robert Downey Jr."};
+        ActorsList.getItems().addAll(actors);
+    }
 }
