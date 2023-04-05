090245cebc3b702bf440bf9f42e80ce227488fcb
LICHG4E <jasserhamdi17@gmail.com>
2023-04-06 00:55:31 +0100

    also styling

--- src/main/java/Controllers/HomePageController.java
+++ src/main/java/Controllers/HomePageController.java
@@ -7,12 +7,15 @@ import Entities.Text;
 import com.example.netflixproject.HelloApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
+import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -26,6 +29,8 @@ public class HomePageController implements Initializable {
     public HBox ThumbnailViewer;
     public Button ProfileBtn;
     public ImageView TopWatched;
+    public AnchorPane ImageAnchor=new AnchorPane();
+
 
 
     @FXML
@@ -41,15 +46,16 @@ public class HomePageController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        String imagePath = "file:src/main/java/Test/LionTest.jpeg";
+        String imagePath = "file:src/main/resources/Images/battlefield-2042-kaleidoscope-map-pc-games-xbox-series-x-3440x1440-6956.jpg";
         TopWatched.setImage(new Image(imagePath));
+        TopWatched.setCursor(Cursor.cursor("hand"));
 
         List<Episode> episodes = new ArrayList<>();
         Resume resume = new Text();
         ((Text) resume).setTexte("This is an Episode");
 
         File file = new File("src/main/java/Test/VideoTest.mp4");
-        File imageFile = new File("src/main/java/Test/LionTest.jpeg");
+        File imageFile = new File("src/main/resources/Images/battlefield-2042-kaleidoscope-map-pc-games-xbox-series-x-3440x1440-6956.jpg");
         Resume resume2 = new Synopsis(file);
         Episode episode = new Episode(1, "EpisodeFamilyGuy", 5, LocalDate.of(2012, 10, 12),
                 LocalDate.of(2012, 10, 10), resume, 1500, 50, 150, file, imageFile);
@@ -62,6 +68,7 @@ public class HomePageController implements Initializable {
                 ImageView imageView = null;
                 try {
                     imageView = new ImageView(String.valueOf(epi.getImage().toURL()));
+                    imageView.setCursor(Cursor.cursor("hand"));
                     imageView.setFitHeight(100);
                     imageView.setFitWidth(150);
                     imageView.setOnMouseClicked(this::handleImageClick);
