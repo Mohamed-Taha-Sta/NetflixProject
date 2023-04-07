97df15efca1cfafd4b5c42cd097d97018aeeebd6
Taha <mohamedtahasta@gmail.com>
2023-04-07 00:48:32 +0000

    Serie DAO w inshallah demain nkamal les acteurs mte3ha

--- src/main/java/DAO/SerieDAO.java
+++ src/main/java/DAO/SerieDAO.java
@@ -1,15 +1,17 @@
 package DAO;
 
+import Entities.Episode;
 import Entities.Season;
 import Entities.Serie;
 import Utils.ConxDB;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SerieDAO {
@@ -67,12 +69,44 @@ public class SerieDAO {
         while (rs.next()) {
 
             long ID = rs.getLong("ID_SERIE");
-            String Name = rs.getString("NAME");
             String Director = rs.getString("DIRECTOR");
             Date DebutDate = rs.getDate("DEBUT_DATE");
-            String Language = rs.getString("ID_SERIE");
+            String Language = rs.getString("LANGUAGE");
+            String Country = rs.getString("COUNTRY");
+            Blob Thumbnail = rs.getBlob("IMAGE");
+            int numsSeasons= rs.getInt("NUMS_SEASONS");
+            String StringGenre = rs.getString("LISTEGENRE");
+            String[] genreArray = StringGenre.split(",");
+            ArrayList<String> genreList = new ArrayList<>(Arrays.asList(genreArray));
+            InputStream SerieThumbnail = Thumbnail.getBinaryStream();
+            InputStream SerieSynopsis = rs.getBinaryStream("SYNOPSIS");
 
+            //Converting Blob Image to Jpeg File
+            File fileThumb = new File("src/main/java/Test/ImgSerie"+ID+".jpeg");
+            OutputStream outS = new FileOutputStream(fileThumb);
+            byte[] bufferImg = new byte[1024];
+            int length;
+            while ((length = SerieThumbnail.read(bufferImg)) != -1) {
+                outS.write(bufferImg, 0, length);
+            }
 
+            //Converting Blob Synopsis to video File .mp4
+            Path outputFilePathSynopsis = Paths.get("src/main/java/Test/SynopsisSerie"+ID+".mp4");
+            try (OutputStream outputStreamSynopsis = Files.newOutputStream(outputFilePathSynopsis)) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = SerieSynopsis.read(buffer)) != -1) {
+                    outputStreamSynopsis.write(buffer, 0, bytesRead);
+                }
+            } catch (IOException e) {
+                System.out.println("Error Handelling the Synopsis");
+            }
+            File fileSynopsis = new File("src/main/java/Test/SynopsisSerie"+ID+".mp4");
+            File fileThumbnail = new File("src/main/java/Test/ImgSerie"+ID+".jpeg");
+
+            List<Season> seasonList = SeasonDAO.FindSeasonSerieID(ID);
+
+//            serie = new Serie(ID,SerieName,Director,DebutDate,Language,Country,genreList,fileThumbnail,numsSeasons,fileSynopsis,seasonList,);
 
         }
 
